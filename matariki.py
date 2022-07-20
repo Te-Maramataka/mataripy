4fd83c8d599b4a6c2958155d5c7d1e964ab67edb
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-20 21:44:01 +1200

    getLunarMonthLength

--- matariki.py
+++ matariki.py
@@ -1,11 +1,16 @@
 from timeutils import *
 from newmoon import getNewMoonAround
+import functools
+import operator
 
 
-def getNewMoonAroundMatariki(y):
-    t = arrow.get(datetime(y, 6, 26), TIMEZONE)
+def getNewMoonAroundMatariki(year):
+    t = arrow.get(datetime(year, 6, 26), TIMEZONE)
     return getNewMoonAround(t)
 
+def getLunarMonthLength(newMoons):
+    time_delta = functools.reduce(operator.sub, map(lambda x: x.floor('day'), newMoons[:2]))
+    return -time_delta.days
 
 if __name__=='__main__':
     print(getNewMoonAroundMatariki(2028))
