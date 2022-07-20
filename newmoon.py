9b8262da380c70a57014559177567d55fb7333e3
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-20 14:01:06 +1200

    fail unification attempt for day

--- newmoon.py
+++ newmoon.py
@@ -3,26 +3,35 @@ from scipy.signal import find_peaks
 from timeutils import *
 
 
-t = arrow.get(datetime(2022,6,26), TIMEZONE)
-
 def findNewMoon(start, stop, step):
     h = Horizons(id='301', location='geocentric', \
                  epochs={'start': arrow2str(start), 'stop': arrow2str(stop), 'step': step})
     v = h.ephemerides(quantities='24')
     idx = find_peaks(v['alpha'])[0]
-    return list(map(str2arrow, v['datetime_str'][idx]))
+    l = list(map(str2arrow, v['datetime_str'][idx]))
+    return l if step=='1d' else l[0]
     # return find_peaks(v['alpha'])[0]
 
 def findNewMoonRefine(date, scale='hour'):
     delta = 60
     if scale == 'minute':
         step = '1m'
-    else:
+    elif scale == 'hour':
         step = '1h'
         delta = delta*24
-    return findNewMoon(date.shift(minutes=-delta), date.shift(minutes=delta), step=step)[0]
+    else:
+        step = '1d'
+        delta = delta*24*30
+    return findNewMoon(date.shift(minutes=-delta), date.shift(minutes=delta), step=step)
 
-v = findNewMoon(t.shift(days=-30), t.shift(days=30), '1d')
-for s in ['hour', 'minute']:
-    v = list(map(lambda x: findNewMoonRefine(x, s), v))
+v = [arrow.get(datetime(2022,6,26), TIMEZONE)]
 
+# v = findNewMoonRefine(v, 'day')
+# v = findNewMoonRefine(v.shift(days=-30), v.shift(days=30), '1d')
+#for s in ['hour', 'minute']:
+#    v = list(map(lambda x: findNewMoonRefine(x, s), v))
+
+#for s in ['day', 'hour', 'minute']:
+#    v = list(map(lambda x: findNewMoonRefine(x, s), v))
+
+v = list(map(lambda x: findNewMoonRefine(x, 'day'), v))
