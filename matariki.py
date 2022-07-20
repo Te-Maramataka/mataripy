38f5931f609e05457c902ac5f6acc684f322191c
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-20 22:24:33 +1200

    getMatarikiTangaroa

--- matariki.py
+++ matariki.py
@@ -12,5 +12,10 @@ def getLunarMonthLength(newMoons):
     time_delta = functools.reduce(operator.sub, map(lambda x: x.floor('day'), newMoons[:2]))
     return -time_delta.days
 
+def getMatarikiTangaroa(year):
+    new_moons = getNewMoonAroundMatariki(year)
+    print("Tangaroa: {},\tLunar Month Length: {}".format(new_moons[0].shift(days=22), getLunarMonthLength(new_moons)))
+
 if __name__=='__main__':
-    print(getNewMoonAroundMatariki(2028))
+    for y in range(2022,2053):
+        getMatarikiTangaroa(y)
