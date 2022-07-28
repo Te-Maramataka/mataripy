bcf88c44277a4f4f5090788bca563526744ec597
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-28 21:29:23 +1200

    sample outputs for both Auckland and geocentric observers

--- matariki.py
+++ matariki.py
@@ -29,10 +29,16 @@ def getMatarikiFriday(year, observer='geocentric'):
 
 if __name__=='__main__':
     # obs = '467' # 'Auckland Observatrory'
-    obs = '500' # 'geocentric'
-    for y in range(2022,2053):
-        new_moons = getNewMoonAroundMatariki(y, observer=obs)
-        t = getMatarikiTangaroa(y, new_moons, observer=obs)
-        m = getMatarikiFriday(y, observer=obs)
-        print("Tangaroa: {}, {}, matariki: {}, Lm: {}"
-              .format(t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b'), getLunarMonthLength(new_moons)))
+    # obs = '500' # 'geocentric'
+    obsrs = ['467', '500']
+    for obs in obsrs:
+        print()
+        print('****************************************')
+        print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
+        print('****************************************')
+        for y in range(1990, 2991, 10):
+            new_moons = getNewMoonAroundMatariki(y, observer=obs)
+            t = getMatarikiTangaroa(y, new_moons, observer=obs)
+            m = getMatarikiFriday(y, observer=obs)
+            print("Tangaroa: {}, {}, matariki: {}"
+              .format(t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')))
