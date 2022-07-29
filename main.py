02fe02ec281235905ddfb36dea7a6c0d9f1c7d69
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-29 20:02:42 +1200

    adding main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+from matariki import *
+
+def displayMatarikiHolidays(years, observers=['500']):
+    for obs in observers:
+        print()
+        print('****************************************')
+        print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
+        print('****************************************')
+        for y in years:
+            new_moons = getNewMoonAroundMatariki(y, observer=obs)
+            t = getMatarikiTangaroa(y, new_moons, observer=obs)
+            m = getMatarikiFriday(y, observer=obs)
+            print("Tangaroa: {}, {}, matariki: {}"
+              .format(t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')))
+
+if __name__=='__main__':
+    displayMatarikiHolidays(range(2022,2053))
