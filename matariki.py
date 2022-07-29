828458756218af54e6e13859d63f90cd4d30db3e
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-29 20:15:47 +1200

    removing main in matariki.py

--- matariki.py
+++ matariki.py
@@ -31,17 +31,4 @@ def getMatarikiFriday(year, newMoons=None, tangaroa=None, observer='geocentric')
     return m
 
 if __name__=='__main__':
-    # obs = '467' # 'Auckland Observatrory'
-    # obs = '500' # 'geocentric'
-    obsrs = ['467', '500']
-    for obs in obsrs:
-        print()
-        print('****************************************')
-        print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
-        print('****************************************')
-        for y in range(1990, 2991, 10):
-            new_moons = getNewMoonAroundMatariki(y, observer=obs)
-            t = getMatarikiTangaroa(y, new_moons, observer=obs)
-            m = getMatarikiFriday(y, observer=obs)
-            print("Tangaroa: {}, {}, matariki: {}"
-              .format(t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')))
+    pass
