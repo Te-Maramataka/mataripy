c223be90f6793872a8d413a6a816f5122f4f8e41
Jinjun Liang <kumkeejjl@gmail.com>
2022-07-29 20:39:03 +1200

    reproducing issue #1

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 from matariki import *
 
-def displayMatarikiHolidays(years, observers=['500'], displayMonthLenght=False):
+def displayMatarikiHolidays(years, observers=['500'], displayMonthLength=False):
+    divider = '****************************************'
     for obs in observers:
         print()
-        print('****************************************')
+        print(divider)
         print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
-        print('****************************************')
+        print(divider)
         lineTemplate = "Tangaroa: {} {}, matariki: {}"
-        if displayMonthLenght:
+        if displayMonthLength:
             lineTemplate += ", Lm: {}"
         for y in years:
             new_moons = getNewMoonAroundMatariki(y, observer=obs)
             t = getMatarikiTangaroa(y, new_moons, observer=obs)
             m = getMatarikiFriday(y, newMoons=new_moons, tangaroa=t, observer=obs)
             lineContent = [t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')]
-            if displayMonthLenght:
+            if displayMonthLength:
                 lineContent.append(getLunarMonthLength(new_moons))
             print(lineTemplate.format(*lineContent))
+        print(divider)
 
 if __name__=='__main__':
-    displayMatarikiHolidays(range(2022,2053), displayMonthLenght=True)
+    displayMatarikiHolidays([2577,2578,2579], displayMonthLength=True)
