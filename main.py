19c38a9ad4eb1f08fa794b27f99d2f5359dbce28
Jinjun Liang <kumkeejjl@gmail.com>
2022-08-02 21:21:03 +1200

    adding simpleMode

--- main.py
+++ main.py
@@ -1,24 +1,29 @@
 from matariki import *
 
-def displayMatarikiHolidays(years, observers=['500'], displayMonthLength=False):
+def displayMatarikiHolidays(years, observers=['500'], displayMonthLength=False, simpleMode=False):
     divider = '****************************************'
     for obs in observers:
-        print()
-        print(divider)
-        print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
-        print(divider)
-        lineTemplate = "Tangaroa: {} {}, matariki: {}"
-        if displayMonthLength:
-            lineTemplate += ", Lm: {}"
+        if not simpleMode:
+            print()
+            print(divider)
+            print("Observer location: {}".format("Auckland Observatrory" if obs == '467' else "geocentric"))
+            print(divider)
+            lineTemplate = "Tangaroa: {} {}, matariki: {}"
+            if displayMonthLength:
+                lineTemplate += ", Lm: {}"
         for y in years:
             new_moons = getNewMoonAroundMatariki(y, observer=obs)
             t = getMatarikiTangaroa(y, new_moons, observer=obs)
             m = getMatarikiFriday(y, newMoons=new_moons, tangaroa=t, observer=obs)
-            lineContent = [t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')]
-            if displayMonthLength:
-                lineContent.append(getLunarMonthLength(new_moons))
-            print(lineTemplate.format(*lineContent))
-        print(divider)
+            if simpleMode:
+                print(m.isoformat().split('T')[0])
+            else:
+                lineContent = [t.strftime('%Y %d %b'), t.strftime('%a'), m.strftime('%d %b')]
+                if displayMonthLength:
+                    lineContent.append(getLunarMonthLength(new_moons))
+                print(lineTemplate.format(*lineContent))
+        if not simpleMode:
+            print(divider)
 
 if __name__=='__main__':
-    displayMatarikiHolidays(range(2022, 2053), displayMonthLength=True)
+    displayMatarikiHolidays(range(1990, 2991, 10), observers=['467'], displayMonthLength=True, simpleMode=True)
