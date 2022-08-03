9240cc0f4e057172185b6c22fe9b693d20855b4c
Jinjun Liang <kumkeejjl@gmail.com>
2022-08-03 16:22:11 +1200

    adding Jalja

--- judging/getAccuracy.py
+++ judging/getAccuracy.py
@@ -1,18 +1,41 @@
 import arrow
+import numpy as np
+from functools import reduce
 
-TEAMS = ['EV']
+TEAMS = ['EV', 'Jalja']
 TEAM_PREDITION_DECADE = '/everydecade.txt'
 TARGET_PRDITION_DECADE = 'sample_decades.txt'
 
-TEAM_FORMATS = {'EV': 'Do MMMM YYYY'}
+TEAM_FORMATS = {'EV': 'Do MMMM YYYY',
+                'Jalja': 'M/D/YYYY'}
 
 def getDatesFromFile(filename, format='YYYY-MM-DD'):
     with open(filename) as f:
         dates = [arrow.get(l.rstrip(), format) for l in f]
     return dates
 
-for team in TEAMS:
-    targets = getDatesFromFile(TARGET_PRDITION_DECADE)
-    predictions = getDatesFromFile(team + TEAM_PREDITION_DECADE, TEAM_FORMATS[team])
-    print(targets[0])
-    print(predictions[0])
+
+def calcAccuracy(x, y):
+    return sum(x == y) / len(y)
+
+
+def getYear(d):
+    return d.datetime.year
+
+
+def getYearList(datelist):
+    return np.asarray(list(map(getYear, datelist)))
+
+
+def getAccuracy():
+    accuracy = {}
+    for team in TEAMS:
+        targets = getDatesFromFile(TARGET_PRDITION_DECADE)
+        predictions = getDatesFromFile(team + TEAM_PREDITION_DECADE, TEAM_FORMATS[team])
+        targets = np.asarray(targets)
+        predictions = np.asarray(predictions)
+        accuracy[team] = calcAccuracy(predictions, targets), calcAccuracy(getYearList(predictions), getYearList(targets))
+    return accuracy
+
+if __name__=='__main__':
+    print(getAccuracy())
